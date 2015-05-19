13f04d68bdf1cf408e4e09f4a739d2136d8e7e83
ZanyGnu <ajaymartin@gmail.com>
2015-05-18 22:35:07 -0700

    working method to retrieve tfs work items created today

--- TfsApp/Program.cs
+++ TfsApp/Program.cs
@@ -1,61 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Text;
-using Microsoft.TeamFoundation.Client;
-using Microsoft.TeamFoundation.Framework.Client;
-using Microsoft.TeamFoundation.Framework.Common;
-using Microsoft.TeamFoundation.WorkItemTracking.Client;
-
+﻿
 namespace TfsApp
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Microsoft.TeamFoundation.Client;
+    using Microsoft.TeamFoundation.Framework.Client;
+    using Microsoft.TeamFoundation.Framework.Common;
+    using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
     class Program
     {
         static void Main(string[] args)
         {
-            // Connect to Team Foundation Server
-            //     server is the name of the server that is running the Team Foundation application-tier.
-            //     port is the port that Team Foundation uses. The default is port is 8080.
-            //     vpath is the virutal path to the Team Foundation application. The default path is tfs.
             TfsConfigurationServer configurationServer =
                 TfsConfigurationServerFactory.GetConfigurationServer(new Uri("http://vstfrd:8080/"));
 
-            // Get the catalog of team project collections
-            CatalogNode catalogNode = configurationServer.CatalogNode;
-            ReadOnlyCollection<CatalogNode> tpcNodes = catalogNode.QueryChildren(
-                new Guid[] { CatalogResourceTypes.ProjectCollection },
-                false, CatalogQueryOptions.None);
-
-            // List the team project collections
-            foreach (CatalogNode tpcNode in tpcNodes)
+            foreach(var collection in GetCollections(configurationServer))
             {
-                // Use the InstanceId property to get the team project collection
-                Guid tpcId = new Guid(tpcNode.Resource.Properties["InstanceId"]);
-                TfsTeamProjectCollection tpc = configurationServer.GetTeamProjectCollection(tpcId);
-
-                // Print the name of the team project collection
-                Console.WriteLine("Collection: " + tpc.Name);
-
-                // Get catalog of team projects for the collection
-                ReadOnlyCollection<CatalogNode> tpNodes = tpcNode.QueryChildren(
-                    new Guid[] { CatalogResourceTypes.TeamProject },
-                    false, CatalogQueryOptions.None);
-
-                // List the team projects in the collection
-                foreach (CatalogNode tpNode in tpNodes)
-                {
-                    Console.WriteLine("Team Project: " + tpNode.Resource.DisplayName);
-                }
-
-                WorkItemStore workItemStore = tpc.GetService<WorkItemStore>();
-
-                //WorkItemCollection workItemCollection = workItemStore.Query(
-                // " SELECT [System.Id], [System.WorkItemType]," +
-                // " [System.State], [System.AssignedTo], [System.Title] " +
-                // " FROM WorkItems " +
-                // //" WHERE [System.TeamProject] = '" + teamProject.Name +
-                //"' ORDER BY [System.WorkItemType], [System.Id]");
+                Console.WriteLine("{0} {1}", collection.Key, collection.Value);
             }
+
+            WorkItemStore workItemStore = configurationServer.GetService<WorkItemStore>();
+
+            var collectionUri = new Uri("http://vstfrd:8080/Azure/");
+            var projectCollection = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(collectionUri);
+            workItemStore = projectCollection.GetService<WorkItemStore>();
+
+            WorkItemCollection workItemCollection = workItemStore.Query(
+                 " SELECT [System.Id], [System.WorkItemType]," +
+                 " [System.State], [System.AssignedTo], [System.Title] " +
+                 " FROM WorkItems " +
+                " WHERE [System.TeamProject] = 'RD'" +
+                " AND [System.CreatedDate] = @Today" +
+                " ORDER BY [System.WorkItemType], [System.Id]");
+
+        }
+
+        public static IList<KeyValuePair<Guid, String>> GetCollections(TfsConfigurationServer configurationServer)
+        {
+            //ApplicationLogger.Log("Entered into GetCollections() : ");
+            var collectionList = new List<KeyValuePair<Guid, String>>();
+            try
+            {
+                configurationServer.Authenticate();
+
+                ReadOnlyCollection<CatalogNode> collectionNodes = configurationServer.CatalogNode.QueryChildren(
+                    new[] { CatalogResourceTypes.ProjectCollection },
+                    false,
+                    CatalogQueryOptions.None);
+                foreach (CatalogNode collectionNode in collectionNodes)
+                {
+                    var collectionId = new Guid(collectionNode.Resource.Properties["InstanceId"]);
+                    TfsTeamProjectCollection teamProjectCollection =
+                        configurationServer.GetTeamProjectCollection(collectionId);
+
+                    if (teamProjectCollection == null)
+                        continue;
+
+                    collectionList.Add(new KeyValuePair<Guid, String>(collectionId, teamProjectCollection.Name));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+            }
+
+            return collectionList;
         }
     }
 }
